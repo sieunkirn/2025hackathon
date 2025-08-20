aa4912e7a1da8cbb78f04992c90c5de549409e42
김시은 <sieunkirn@gmail.com>
2025-08-20 21:25:46 +0900

    vercel 체크

--- src/lib/apiClient.js
+++ src/lib/apiClient.js
@@ -1,11 +1,24 @@
-// src/lib/apiClient.js
-console.log("API BASE =", import.meta.env.VITE_API_BASE_URL);
+// // src/lib/apiClient.js
+// console.log("API BASE =", import.meta.env.VITE_API_BASE_URL);
 
+// import axios from 'axios';
+
+// const api = axios.create({
+//     baseURL: import.meta.env.VITE_API_BASE_URL,
+//     timeout: 15000,
+// });
+
+// export default api;
+// src/lib/apiClient.js
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL,
-    timeout: 15000,
+  baseURL: window.location.hostname.includes("vercel.app")
+    ? "https://d32cc7c8eb4b.ngrok-free.app" // 배포일 때
+    : "http://localhost:8080",              // 로컬일 때
+  timeout: 15000,
 });
+
+console.log("API BASE =", api.defaults.baseURL);
 
 export default api;
